62ceab5bcf53656e9f6e905ef7a7acbecf26c5a7
vusalindahouse <vusi444@gmail.com>
2022-05-31 17:22:01 +0300

    добавление легкого уровня бота

--- ai.py
+++ ai.py
@@ -1,1 +1,13 @@
 from field import FIELD
+from random import randrange
+import field
+
+
+def easy_lev():
+    while True:
+        y, x = randrange(field.DIM), randrange(field.DIM)
+        if not field.DIM[y][x]:
+            return False
+        return [y][x]
+
+
